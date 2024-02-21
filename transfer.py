81bb8379a3c308277222c7fac7f04dcc2c702029
acsii-63 <duy.nq203690@sis.hust.edu.vn>
2024-02-21 14:46:59 +0700

    timestamp format fix

--- transfer.py
+++ transfer.py
@@ -234,14 +234,14 @@ def getTimestampFromMessage(_message):
 def convertUTC0ToUTC7(timestamp):
     """Convert ts str from UTC+0 to UTC+7"""
 
-    dt_utc0 = datetime.strptime(timestamp, '%Y-%m-%dT%H:%M:%S.%fZ')
+    dt_utc0 = datetime.strptime(timestamp, '%Y-%m-%dT%H:%M:%S.%f')
 
     utc0 = timedelta(hours=0)
     utc7 = timedelta(hours=7)
     dt_utc7 = dt_utc0 + (utc7 - utc0)
 
-    timestamp_utc7 = dt_utc7.strftime('%Y-%m-%dT%H:%M:%S.%fZ')
-    final_timestamp = timestamp_utc7[0:-4] + timestamp_utc7[-1]
+    timestamp_utc7 = dt_utc7.strftime('%Y-%m-%dT%H:%M:%S.%f')
+    final_timestamp = timestamp_utc7[0:-4]
 
     return final_timestamp
 
